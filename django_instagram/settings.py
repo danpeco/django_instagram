8020193024c4275b26619baeee49dc44638e12a8
danpeco <dperez88@gmail.com>
2021-12-12 14:15:03 -0500

    added login and logout auth views

--- django_instagram/settings.py
+++ django_instagram/settings.py
@@ -153,7 +153,9 @@ STATICFILES_FINDERS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_URL = 'login'
+LOGIN_URL = 'users/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
 '''Users views'''
 
 # Django
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
 from django.views.generic import DetailView, FormView, UpdateView
 from django.urls import reverse, reverse_lazy
 
@@ -67,26 +66,16 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         username = self.object.user.username
         return reverse('users:detail', kwargs={'username': username})
 
-def login_view(request):
+
+class LoginView(auth_views.LoginView):
     '''Login view.'''
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username or password'})
-    return render(request, 'users/login.html')
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
 
 
-@login_required
-def logout_view(request):
-    '''Logout a user'''
-    
-    logout(request)
-    return redirect('users:login')
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    '''Logout view.'''
 
+    pass
 
